05961e860250727fe2b4d1ddc859445ef7b51e76
Max Kim <31351608+Solar-Flare@users.noreply.github.com>
2021-06-17 19:48:10 +0700

    Navbar Overhaul

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("navbar").innerHTML = `<div class='row' style='width: 100%; border: 1px solid red;'>
+  <button class='btn btn-primary m-1'>Home</button>
+    <button class="btn btn-primary m-1">About Me</button>
+    <button class="btn btn-primary m-1">Gallery</button>
+</div>`;

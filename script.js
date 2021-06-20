e177a85563ff808947ce2a91c014bd23e706f732
Son Nguyen <Son.mofa415@gmail.com>
2021-06-21 06:51:50 +0700

    yes

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.getElementById("navbar").innerHTML = `<div class='row d-flex justify-content-center' style='width: 100%; border: 1px solid red;'>
   <a href="index.html"><button class='btn btn-primary m-1' >Home</button></a>
-  <a href="aboutme.html"><button class="btn btn-primary m-1">About Me</button></a>
+  <a href="about.html"><button class="btn btn-primary m-1">About Me</button></a>
   <a href="gallery.html"><button class="btn btn-primary m-1">Gallery</button></a>
 </div>`;
 
 
 
-document.getElementById("footer").innerHTML = `<p>Author: ?????<br>
-<a href="#" class="fa fa-facebook"></a>
+document.getElementById("footer").innerHTML = `<img src="logo.png" alt="Title"><br>
+<a href="#" class="fa fa-instagram"></a>
+<a href="tel:123-456-7890">123-456-7890</a>
 <br>
 <a href="mailto:email@example.com">email@example.com</a></p>`;
